a7297d44b4b44499f0b4de9c0edcdc6bfbbc18d1
Xazaxax <s4206@bk.ru>
2023-11-11 19:51:51 +0400

    import moduls

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import pygame.key
 from pygame.sprite import Sprite
 from pygame import Surface, image, transform
 import random
+import config
+running = True
+
+
+screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
+clock = pygame.time.Clock()
+
+image = image.load('assets/karta.png')
+rect = image.get_rect()
+
+while running:
+    clock.tick(config.FRAMERATE)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
